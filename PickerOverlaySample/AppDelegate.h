9dd358aa1b1c731a0e2afa6798bd838dc7ef5d62
Leonty Deriglazov <leon@equality.ru>
2012-04-17 12:40:28 +0700

    Initial Commit

---
+++ PickerOverlaySample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PickerOverlaySample
+//
+//  Created by Leonty Deriglazov on 17.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PickerOverlaySample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PickerOverlaySample
+//
+//  Created by Leonty Deriglazov on 17.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

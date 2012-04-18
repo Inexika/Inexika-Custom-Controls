2b9b50a703bfd89808d2e8773d93aa6307491c1a
Leonty Deriglazov <leon@equality.ru>
2012-04-18 18:40:47 +0700

    Add copyright notices.

--- PickerOverlaySample/AppDelegate.m
+++ PickerOverlaySample/AppDelegate.m
@@ -3,7 +3,7 @@
 //  PickerOverlaySample
 //
 //  Created by Leonty Deriglazov on 17.04.12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Inexika, http://www.inexika.com All rights reserved.
 //
 
 #import "AppDelegate.h"
